1246f10f49507bbd5bf8299c318793c3b31b7728
hobsonr <37401298+hobsonr@users.noreply.github.com>
2018-05-16 08:33:53 +0100

    Add files via upload
    now using file input stream instead of buffered reader, putting words and counter into separate array lists
    can read one file at a time and count the words
    however still counts punctuation

--- driver.java
+++ driver.java
@@ -1,27 +1,43 @@
 //driver class launches the main method
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.util.*; //for scanner and arraylist
 import java.io.IOException;
 
 public class driver
 {
-   public static void main(String[] args)
+   public static void main(String[] args) throws IOException
    {
-	   String file1 = "test1.txt";
-	   try{
-		   FileReader file = new FileReader(file1);
-		   BufferedReader in = new BufferedReader(file);
-       
-           String line = in.readLine();
-           while (line != null)
-           {
-		       line = in.readLine();
-		       System.out.println(file1);
-		       }
-		   }
-		       catch (IOException e){
-				    e.printStackTrace();
-			 
-	    }
-    }
+      //create input stream and scanner
+      FileInputStream f = new FileInputStream("test1.txt");
+      Scanner file = new Scanner(f);
+
+      //Create the Arraylists
+      ArrayList<String> wordcount = new ArrayList<String>();
+      ArrayList<Integer> counter = new ArrayList<Integer>();
+
+      //Read through file and find words
+      while(file.hasNext()){
+        //get next word
+        String nextWord = file.next();
+        //determine if the word is in the array list already
+        if(wordcount.contains(nextWord)){
+          //find the index the word is located at, goes to counter array list, increases counter by 1 each time the word is found
+          int index = wordcount.indexOf(nextWord);
+          counter.set(index, counter.get(index)+1);
+          }
+        //if a new word has been found, set the counter to 1
+        else{
+          wordcount.add(nextWord);
+          counter.add(1);
+          }
+        }
+        f.close();
+        file.close();
+
+        //print the results
+        for(int i = 0; i < wordcount.size(); i++){
+          System.out.println(wordcount.get(i)+" occured "+ counter.get(i) + " time(s)");
+          }
+}
 }
